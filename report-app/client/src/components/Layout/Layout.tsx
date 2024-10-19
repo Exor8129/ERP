4bf860c10eead6963879f6043660884ecd87fef1
Exor8129 <exormedicalsystems@gmail.com>
2024-10-19 22:30:33 +0530

    profile-section

--- report-app/client/src/components/Layout/Layout.tsx
+++ report-app/client/src/components/Layout/Layout.tsx
@@ -1,27 +1,51 @@
-// Layout.tsx
 import { Outlet } from "react-router-dom";
 import "./layout.css";
-import {LoaderPinwheel, Menu } from 'lucide-react';
-
+import {  ArrowRightLeft, ChevronRight, LoaderPinwheel, MessageSquare } from 'lucide-react';
+import { useState } from "react";
 
 const Layout = () => {
+  const [open, setOpen] = useState(false);
+  
+  const toggleSidenav = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
   return (
     <div className="layout-container">
-      <nav className="sidenavi">
-        <div className="top-section">
-            <div className="logo">
-                <LoaderPinwheel/>
-            </div>
-            <div className="logo-name">
-            <span>Now</span><span>ERP</span>
-            
-            </div>
-            <div className="menu">
-            <Menu/>
-            </div>
-
-        </div>
+      <nav className={`sidenavi ${open ? 'open' : 'closed'}`}>
+        {/* Top section code starts here */}
         
+        <div className="top-section">
+          <div className="logo">
+            <LoaderPinwheel size={34} strokeWidth={1.7} onClick={toggleSidenav}/>
+          </div>
+          <div className={`logo-name ${open ? 'visible' : 'hidden'}`}>
+            <span className="name">Now</span><span className="name2">ERP</span>
+          </div>
+          <div className={`menu ${open?'open' : 'closed'}`}>
+            <ChevronRight size={34} onClick={toggleSidenav} />
+          </div>
+        </div>
+        {/* Top section code ends here */}
+        
+
+        {/* Business-Profile section code starts here */}
+
+          <div className="business-container">
+            <div className="business-logo">
+              logo
+            </div>
+          </div>
+          <div className="business-icons">
+                <MessageSquare/>
+                <ArrowRightLeft/>
+                
+
+              </div>
+
+        {/* Business-Profile section code ends here */}
+        
+
       </nav>
       <main className="main-content">
         <Outlet /> {/* This will render the current page content */}
